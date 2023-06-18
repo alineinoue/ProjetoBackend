cf72dd3d268c5ba23857813f72b31143e2b75637
Aline <alineinouecerqueira@hotmail.com>
2023-06-18 17:02:06 -0300

    Métodos do ProductController

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -44,6 +44,17 @@ module.exports = {
             return;
         }
 
+        if(cat.length < 12){
+            res.json({error: 'ID de categoria inválido!'});
+            return;
+        }
+
+        const category = await Category.findById(cat);
+        if(!category){
+            res.json({error: 'Categoria não existe'});
+            return;
+        }
+
         //Formatação do preço
         if(price){
             price = price.replace('.', '').replace(',', '.').replace('R$', '');
@@ -91,12 +102,188 @@ module.exports = {
         res.json({id: info._id});
     },
     getList: async (req, res) => {
+        let {limit = 5, q, cat, page } = req.query;
+        let total = 0;
+        let filters = {}; 
+    
+        if (q) {
+            filters.title = { '$regex': q, '$options': 'i' };
+        }
+    
+        if (cat) {
+            const c = await Category.findOne({ slug: cat }).exec();
+            if (c) {
+                filters.category = c._id.toString();
+            }
+        }
+    
+        const productsTotal = await Product.find(filters).exec();
+        total = productsTotal.length;
 
-    },
+        const startIndex = (page - 1) * limit;
+
+        const productsData = await Product.find(filters)
+            .skip(startIndex)
+            .limit(parseFloat(limit))
+            .exec();
+    
+        let products = [];
+    
+        for (let i in productsData) {
+            let image;
+    
+            let defaultImg = productsData[i].images.find(e => e.default);
+            if (defaultImg) {
+                image = `${process.env.BASE}/media/${defaultImg.url}`;
+            } else {
+                image = `${process.env.BASE}/media/default.jpg`;
+            }
+    
+            products.push({
+                id: productsData[i]._id,
+                title: productsData[i].title,
+                price: productsData[i].price,
+                description: productsData[i].description,
+                image
+            });
+        }
+    
+        res.json({ products, total });
+    },    
     getItem: async (req, res) => {
+        let {id, other = null} = req.query;
 
+        if(!id){
+            res.json({error: 'Sem produto'});
+            return;
+        }
+
+        if(id.length < 12){
+            res.json({error: 'ID inválido'});
+            return;
+        }   
+
+        const product = await Product.findById(id);
+        if(!product){
+            res.json({error: 'Produto inexistente'});
+        }
+
+        let images = [];
+        for(let i in product.images){
+            images.push(`${process.env.BASE}/media/${product.images[i].url}`);
+        }
+
+        let category = await Category.findById(product.category).exec();
+        let userInfo = await User.findById(product.idUser).exec();
+
+        let others = [];
+        if(other) {
+            const otherData = await Product.find({idUser: product.idUser}).exec();
+
+            for(let i in otherData){
+                if(otherData[i]._id.toString() != product._id.toString()){
+
+                    let image = `${process.env.BASE}/media/default.jpg`;
+
+                    let defaultImg = otherData[i].images.find(e => e.default);
+
+                    if(defaultImg){
+                        image = `${process.env.BASE}/media/${defaultImg.url}`;
+                    }
+
+                    others.push({
+                        id: otherData[i]._id,
+                        title: otherData[i].title,
+                        price: otherData[i].price,
+                        description: otherData[i].description,
+                        amount: otherData[i].amount,
+                        image
+                    })
+
+                } 
+            }
+        }
+
+        res.json({
+            id: product._id,
+            title: product.title,
+            price: product.price,
+            description: product.description,
+            amount: product.amount,
+            images,
+            category, 
+            userInfo: {
+                name: userInfo.name,
+                email: userInfo.email
+            },
+            others
+        });
     },
     edit: async (req, res) => {
-
-    }
+        let { id } = req.params;
+        let { title, price, description, amount, images, cat, token } = req.body;
+    
+        if (id.length < 12) {
+            res.json({ error: 'ID inválido' });
+            return;
+        }
+    
+        const product = await Product.findById(id).exec();
+        if (!product) {
+            res.json({ error: 'Produto não encontrado!' });
+            return;
+        }
+    
+        let user;
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            user = await User.findById(decoded.id);
+        } catch (error) {
+            res.json({ error: 'Token inválido' });
+            return;
+        }
+    
+        if (user && user._id.toString() !== product.idUser) {
+            res.json({ error: "Você não tem permissão para modificar este produto" });
+            return;
+        }
+    
+        let updates = {};
+    
+        if (title) {
+            updates.title = title;
+        }
+    
+        if (price) {
+            price = price.replace('.', '').replace(',', '.').replace('R$', '');
+            price = parseFloat(price);
+            updates.price = price;
+        }
+    
+        if (amount) {
+            updates.amount = amount;
+        }
+    
+        if (description) {
+            updates.description = description;
+        }
+    
+        if (cat) {
+            const category = await Category.findOne({ slug: cat }).exec();
+            if (!category) {
+                res.json({ error: 'Categoria não existe' });
+                return;
+            }
+            updates.category = category._id.toString();
+        }
+    
+        if (images) {
+            updates.images = images;
+        }
+    
+        await Product.findByIdAndUpdate(id, { $set: updates });
+    
+        res.json({ error: '' });
+    },
+    
 };
